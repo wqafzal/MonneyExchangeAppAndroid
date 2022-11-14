454cb1cf46e00ff0fd4b2c4135c505ce26c2e8f4
waqar <waqarafz@gmail.com>
2022-11-14 23:28:57 +0500

    Add Hilt dependency

--- app/src/main/java/com/example/moneyexchangeapp/AndroidApp.kt
+++ app/src/main/java/com/example/moneyexchangeapp/AndroidApp.kt
@@ -2,7 +2,9 @@ package com.example.moneyexchangeapp
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class AndroidApp : Application(){
     init {
         instance = this
