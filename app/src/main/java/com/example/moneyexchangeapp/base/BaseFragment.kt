c3000871ad8a2ed7e7c3b1ad462456be352dfa8b
waqar <waqarafz@gmail.com>
2022-11-16 15:51:58 +0500

    define show common function for snackbar messages

--- app/src/main/java/com/example/moneyexchangeapp/base/BaseFragment.kt
+++ app/src/main/java/com/example/moneyexchangeapp/base/BaseFragment.kt
@@ -1,6 +1,18 @@
 package com.example.moneyexchangeapp.base
 
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 
 abstract class BaseFragment : Fragment() {
+    fun showLongSnackbar(message: String, action: (() -> Unit)? = null) {
+        view?.let {
+            Snackbar.make(it, message, Snackbar.LENGTH_LONG).let {
+                if (action != null)
+                    it.setAction(android.R.string.ok) {
+                        action()
+                    }
+                it
+            }.show()
+        }
+    }
 }
